02546d84b2b894c87822f0a80446f4f1490c5900
Nuno Costa <nuno.pinto.costa@gmail.com>
2015-08-13 19:05:55 +0100

    Hives now possess the following properties:
     - breedingcomb count
      - Honeycomb count
    
      The overlay now asks for these values.
    
      TODO: It is still impossible to see these values in the interface

---
+++ app/src/main/java/com/example/bee/beehive/Activities/AddApiaryOverlay.java
@@ -0,0 +1,51 @@
+package com.example.bee.beehive.Activities;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bee.beehive.R;
+
+public class AddApiaryOverlay extends DialogFragment {
+
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		final LayoutInflater inflater = getActivity().getLayoutInflater();
+		final View view = inflater.inflate(R.layout.activity_add_apiary_overlay, null);
+		TextView overlayTitle = (TextView) view.findViewById(R.id.overlayAddApiary);
+
+		builder.setView(view);
+/*
+		final String activity_name = getActivity().getClass().getSimpleName();
+		if (activity_name.equals(HiveActivity.class.getSimpleName())) {
+			overlayTitle.setText("Hive name");
+		}*/
+		builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				EditText input = (EditText) view.findViewById(R.id.ApiaryNameInput);
+
+				((ListItem) getActivity()).add(input.getText().toString());
+
+			}
+		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+			}
+		});
+
+		Dialog dialog=builder.create();
+		return dialog;
+	}
+
+}
